779b8475fa25a543357e63a782746f2d73e3c4ed
Juan <jaramillocarrilloj25@gmail.com>
2023-01-18 14:44:02 +0100

    Event names and dates (well formatted) gotten

--- Agenda/APIExecuter.swift
+++ Agenda/APIExecuter.swift
@@ -7,6 +7,9 @@ class GetterPoster {
         let name: String
     }
     
+    var eventNames = [String]()
+    var valueOfDates = [Any]()
+    
     func send() {
         let urlSession = URLSession.shared
         let url = URL(string: "https://superapi.netlify.app/api/db/eventos")
@@ -16,9 +19,18 @@ class GetterPoster {
             
             if let data = data {
                 let json = try? JSONSerialization.jsonObject(with: data)
+                
                 for i in json as! [[String: Any]] {
-                    print(i.keys)
+                    self.eventNames.append(i["name"] as! String)
+                    self.valueOfDates.append(i["date"]!)
                 }
+                
+                print(self.eventNames)
+                print(self.valueOfDates)
+                
+                let date = NSDate(timeIntervalSince1970: self.valueOfDates[1] as! TimeInterval)
+                print(date)
+                //print(date)
 //                //print(String(describing: json))
 //                let dictJSON = String(describing: json).data(using: .utf8)
 //
